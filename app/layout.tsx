03606b7572a064d0d047c4211771dfeef55c8b85
JLi2007 <LiJames9532@gmail.com>
2025-09-17 20:34:56 -0400

    feat: google analytics | WHY WAS MOCK DATA STILL HERE

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 export const metadata: Metadata = {
   title: "TrivialGuessr",
@@ -17,6 +18,7 @@ export default function RootLayout({
         className={`antialiased font-quicksand`}
       >
         {children}
+        <GoogleAnalytics gaId="G-968C85GP1X" />
       </body>
     </html>
   );
